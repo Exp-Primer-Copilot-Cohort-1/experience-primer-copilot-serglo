1da2d8d7a3d987187aac13e8f5053b61603ee0a3
Serg Lotz <blondewalker@gmail.com>
2024-01-23 15:07:09 +0100

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,82 @@
-const http = require('http');
+// Create web server using express
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, '../data/comments.json');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+// GET /comments
+router.get('/', function(req, res, next) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      var comments = JSON.parse(data);
+      res.json(comments);
+    }
+  });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// POST /comments
+router.post('/', function(req, res, next) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      var comments = JSON.parse(data);
+      var comment = req.body;
+      comment.id = comments.nextId;
+      comments.nextId++;
+      comments.push(comment);
+      fs.writeFile(commentsPath, JSON.stringify(comments), function(err) {
+        if (err) {
+          next(err);
+        } else {
+          res.json(comment);
+        }
+      });
+    }
+  });
 });
 
-// test
+// PUT /comments/:id
+router.put('/:id', function(req, res, next) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      var comments = JSON.parse(data);
+      var comment = comments.filter(function(comment) {
+        return comment.id == req.params.id;
+      })[0];
+      if (comment) {
+        comment.body = req.body.body;
+        fs.writeFile(commentsPath, JSON.stringify(comments), function(err) {
+          if (err) {
+            next(err);
+          } else {
+            res.json(comment);
+          }
+        });
+      } else {
+        res.status(404).json({
+          error: {
+            message: 'Comment with id ' + req.params.id + ' not found'
+          }
+        });
+      }
+    }
+  });
+});
+
+// DELETE /comments/:id
+router.delete('/:id', function(req, res, next) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      var comments = JSON.parse(data);
+      var comment = comments.filter(function(comment) {
+        return comment.id == req.params.id;
+      })[0];
+      if
